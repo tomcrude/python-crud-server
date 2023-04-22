38344224f1be2ce6a14221a476d78084e7c264a6
angelo <angesmor12@hotmail.com>
2023-04-22 01:36:54 -0300

    e9

--- main/settings.py
+++ main/settings.py
@@ -144,6 +144,8 @@ if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
